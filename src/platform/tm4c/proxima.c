552d878aa12d851cbe5cee77ba652e650bcbe25d
Darren Stahl <darren.stahl@live.ca>
2015-03-20 21:07:08 -0400

    Added motor set function

--- src/platform/tm4c/proxima.c
+++ src/platform/tm4c/proxima.c
@@ -111,6 +111,7 @@ const LUA_REG_TYPE proxima_mc_map[] =
     { LSTRKEY("right"), LFUNCVAL( proxima_motors_right ) },
     { LSTRKEY("left"), LFUNCVAL( proxima_motors_left ) },
     { LSTRKEY("stop"), LFUNCVAL( proxima_motors_stop ) },
+    { LSTRKEY("set"), LFUNCVAL( proxima_motors_set ) },
     { LNILKEY, LNILVAL },
 };
 
